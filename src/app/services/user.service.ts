915f5ab064a1bd4e6605909c77fc63ec88cc3507
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-06-06 15:27:06 +0530

    change

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
   
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
   private url = 'https://reqres.in/api/users';
